35d528199f2b778e5a12ba5eed9089a338c466db
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-12-17 09:56:54 +0000

    initialized prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma.service.js';
+import { UserService } from './user.service.js';
+import { PostService } from './post.service.js';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
